5392836bb4724adfe9125c1ad12a3bcab87ea00f
code-sekhar <sekharchaudhary5@gmail.com>
2026-04-27 22:09:23 +0530

    project update

---
+++ lib/pages/todo_page.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
